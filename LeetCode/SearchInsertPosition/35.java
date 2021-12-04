9e2dee54243aeb7a4d1d96833894ede3d4486f69
yeeun0514 <yeeun.g.sin@gmail.com>
2021-12-04 16:05:05 +0900

    [#5] type : solved algorithm body : 35, 36

---
+++ LeetCode/SearchInsertPosition/35.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int size = nums.length;
+        for(int i=0; i<size; i++){
+            if(nums[i] == target || nums[i] > target){
+                return i;
+            }
+        }
+        return size;
+    }
+}
