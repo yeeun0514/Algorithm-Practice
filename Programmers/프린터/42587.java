118378e4b5eba6cd98f440e483092087419a69ee
yeeun0514 <yeeun.g.sin@gmail.com>
2021-12-11 00:59:52 +0900

    [#6] type : solved algorithm body : 42587 프린터

---
+++ Programmers/프린터/42587.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        
+        Queue<Printer> q = new LinkedList<>();
+        
+        for(int i=0; i<priorities.length; i++){
+            q.add(new Printer(priorities[i], i));
+        }
+        
+        while(!q.isEmpty()){
+            Printer cur = q.poll();
+            boolean check = false;
+            for(Printer p : q){
+                if(cur.prior<p.prior){
+                    check = true;
+                    break;
+                }
+            }
+            
+            if(check){
+                q.add(cur);
+            }else{
+                if(cur.location == location){
+                    answer = priorities.length - q.size();
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+    
+    class Printer {
+        int prior;
+        int location;
+        
+        Printer(int prior, int location){
+            this.prior = prior;
+            this.location = location;
+        }
+    }
+}
