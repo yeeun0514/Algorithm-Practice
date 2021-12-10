3cf29584da41e0fe0e89a764062f23e055c1dad0
yeeun0514 <yeeun.g.sin@gmail.com>
2021-12-11 00:51:46 +0900

    [#6] type : solved algorithm body : 72412 순위검색

--- Programmers/순위검색/72412.java
+++ Programmers/순위검색/72412.java
@@ -1,5 +1,3 @@
-package Programmers.순위검색;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -8,36 +6,35 @@ import java.util.List;
 import java.util.Map;
 
 class Solution {
-    public static void main(String[] args) {
-        String[] info = {"java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"};
-        String[] query = {"java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"};
-        Solution s = new Solution();
-        int[] answers = s.solution(info, query);
-        for(int a : answers){
-            System.out.print(a);
-        }
-    }
-
 
     Map <String, List<Integer>> map = new HashMap<String, List<Integer>>();
 
     public int[] solution(String[] info, String[] query) {
         int[] answer = new int[query.length];
 
+        // map에 지원자 경우의 수에 따른 점수 정보를 넣음
+        setInfoMap(info);
+        // 각 경우의 수에 대한 점수를 오름차순으로 정렬
+        sortScore();
+        // 쿼리에 맞는 결과 출력
+        answer = getAnswer(query);
+        
+        return answer;
+    }
+
+    // dfs를 사용하여 map에 지원자 경우의 수에 따른 정보를 넣음
+    public void setInfoMap(String[] info){
         for(String i : info){
             dfs(i.split(" "), "", 0);
         }
-        
-        Iterator<String> ite = map.keySet().iterator();
-        while(ite.hasNext()){
-            String key = ite.next();
-            // List<Integer> temp = map.get(key);
-            Collections.sort(map.get(key));
-        }
+    }
 
+    // query를 key형식에 맞춰 변환 후 정답 찾기
+    public int[] getAnswer(String[] query){
+        int[] answer = new int[query.length];
         for(int i =0; i< query.length; i++){
             String str = query[i];
-            str.replaceAll(" and ", "");
+            str=str.replaceAll(" and ", "");
             String[] qInfo = str.split(" ");
             if(!map.containsKey(qInfo[0])){
                 answer[i] = 0;
@@ -49,6 +46,16 @@ class Solution {
         return answer;
     }
 
+    // 각 경우에 대한 점수들 정렬
+    public void sortScore(){
+        Iterator<String> ite = map.keySet().iterator();
+        while(ite.hasNext()){
+            String key = ite.next();
+            Collections.sort(map.get(key));
+        }
+    }
+
+    // 해당 조건에 맞는 경우 중 점수에 부합하는 갯수 찾기
     public int counter(String str, String score){
         List<Integer> temp = map.get(str);
         int start = 0;
@@ -65,6 +72,7 @@ class Solution {
         return temp.size()-start;
     }
 
+    // dfs 사용하여 모든 info에 대한 모든 경우의수 map에 key로 저장 및 점수 입력
     public void dfs(String[] info, String str, int depth){
         if(depth == 4){
             if(map.containsKey(str)){
