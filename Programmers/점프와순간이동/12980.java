1fc9c502dd31d9815dce60e5e7c560f9a7546f45
yeeun0514 <yeeun.g.sin@gmail.com>
2022-02-25 02:10:09 +0900

    [#6] type : solved algorithm body : 12980 점프와 순간 이동

---
+++ Programmers/점프와순간이동/12980.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int ans = 0;
+        while(true) {
+        	if(n%2==1) {
+        		n--;
+        		ans++;
+        	}else {
+        		n = n/2;
+        	}
+        	if(n==0) {
+        		break;
+        	}
+        }
+        
+        return ans;
+    }
+}
