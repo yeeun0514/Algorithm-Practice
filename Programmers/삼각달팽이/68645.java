a5789bf1a123441d892779a279eb0c379dcff390
yeeun0514 <yeeun.g.sin@gmail.com>
2022-02-25 01:26:44 +0900

    [#6] type : solved algorithm body : 68645 삼각 달팽이

---
+++ Programmers/삼각달팽이/68645.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = {};
+        int[][] triSnail = new int[n][n];
+        
+        int number = 0;
+        int countNum = n;
+        int course = 1;
+        int x=0;
+        int y=-1;
+        
+        while(countNum != 0) {
+        	
+        	for(int i=0; i<countNum; i++) {
+        		if(course%3 == 1) {
+        			y++;
+        		}else if(course%3 == 2) {
+        			x++;
+        		}else {
+        			x--;
+        			y--;
+        		}
+        		number++;
+        		triSnail[y][x] = number;
+        	}
+        	
+    		countNum--;
+    		course++;
+        }
+        
+        answer = new int[number];
+        int index=0;
+        for(int i=0; i<n; i++) {
+        	for(int j=0; j<i+1; j++) {
+        		answer[index] = triSnail[i][j];
+        		index++;
+        	}
+        }
+        
+        return answer;
+    }
+}
