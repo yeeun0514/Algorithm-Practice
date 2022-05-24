815791d45d11e963c414a3436f6ea7a02894ec76
yeeun0514 <yeeun.g.sin@gmail.com>
2022-05-24 13:56:57 +0900

    refactor
    불필요한 내용 삭제
    
    issues : #6, #7

--- Programmers/문자열내마음대로정렬하기/Lesson12915.java
+++ Programmers/문자열내마음대로정렬하기/Lesson12915.java
@@ -12,25 +12,20 @@ public class Lesson12915 {
 		Solution s = new Solution();
 		s.solution(wordArr, 1);
 	}
-
 }
 
 class Solution {
     public List<String> solution(String[] strings, int n) {
         List<String> answer;
-        
         List<Words> words = new ArrayList<>();
+        
         for(String str : strings){
-        	String[] iThArray = str.split("");
-        	String iTh = iThArray[n];
-        	Words w = new Words(str, iTh);
-        	words.add(w);
+        	words.add(new Words(str, str.split("")[n]));
         }
         
         answer = words.stream()
         		.sorted(Comparator.comparing(Words::getiThWord).thenComparing(Words::getWord))
         		.map(Words::getWord)
-        		.peek(System.out::println)
         		.collect(Collectors.toList());
         
         return answer;
